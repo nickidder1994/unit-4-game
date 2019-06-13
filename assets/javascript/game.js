e191c7e4ac36129e99cb6c21700c7017d1c7b723
Nick Kidder <nickidder@yahoo.com>
2019-06-12 18:27:59 -0700

    added variables to javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+var randomResults;
+var win;
+var lose;
+
+console.log("lose")
+console.log("win")
+console.log("randomResults")
+
+for(var i = 0; i < 4; i++) {
+    var crystals = $("<diw>");
+    $(".crystals").append(crystals)
+}
+
+//create a random number generator for all four crystals between 1 - 12
+//regenerate new random numbers every time you win or lose
+//anytime you click on a crystal it should add to toal number
+// create random numbers that the crystals must add up to between 19-12
+//create a way to keep track of wins and loses
+//if number of crysals adds up to a number greater than the number chosen then track loss and restart
+//if number adds up equal to number given than track win
+
+
