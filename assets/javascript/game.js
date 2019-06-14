5d70d2d2772efedfe3098baa5d323fc147b2d679
Nick Kidder <nickidder@yahoo.com>
2019-06-13 23:31:30 -0700

    add random number generators to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,30 @@
 var randomResults;
 var win;
 var lose;
+var targetNumber;
+var crystal1
+var crystal2
+var crystal3
+var crystal4
 
-console.log("lose")
-console.log("win")
-console.log("randomResults")
-
-for(var i = 0; i < 4; i++) {
-    var crystal = $("<diw>");
-    crystal.attr("class", "crystals")
-    $(".crystals").append(crystal)
+function generateRandomNumbers() {
+    targetNumber = Math.floor(Math.random()*120)+1
+    crystal1 = Math.floor(Math.random()*120)+1
+    crystal2 = Math.floor(Math.random()*120)+1
+    crystal3 = Math.floor(Math.random()*120)+1
+    crystal4 = Math.floor(Math.random()*120)+1
 }
+generateRandomNumbers();
+console.log(targetNumber)
+
+
+// for(var i = 0; i < 4; i++){
+    
+//     var crystal = $("<div>");
+//     crystal.attr("class", "crystals");
+    
+//     $(".crystals").append(crystal);
+// }
 
 //create a random number generator for all four crystals between 1 - 12
 //regenerate new random numbers every time you win or lose
