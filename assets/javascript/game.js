fce3a0b218b5ffc5073604e35a60c2ed76522b27
Nick Kidder <nickkidder@Nicks-MacBook-Pro.local>
2019-12-10 18:24:19 -0800

    fixed  code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var randomResults;
 var win;
 var lose;
 var targetNumber;
-var userScore = 0;
+var userScore = addCrystalValues();
 var crystal1
 var crystal2
 var crystal3
@@ -31,36 +31,18 @@ $(".crystal").on("click", function (event){
     var clickedCrystal = event.currentTarget.attributes[1].value
     userScore += parseInt(clickedCrystal)
     console.log(clickedCrystal)
-    console.log(userScore)
+    
 
     // Logic to check if userScore is = to taretNumber
     // Else if userScore is greater than targetNunber game over
     if(userScore === targetNumber) {
-        console.log("You Win")
+        alert("You Win!!!")
     } else if (userScore > targetNumber) {
-        console.log("You lose")
+        alert("You Lose!!!")
     }
 
 });
 $(".random-number-box").append(targetNumber)
-$("value-box").append(crystal1)
-
-
-
-// for(var i = 0; i < 4; i++){
-
-//     var crystal = $("<div>");
-//     crystal.attr("class", "crystals");
-
-//     $(".crystals").append(crystal);
-// }
-
-//create a random number generator for all four crystals between 1 - 12
-//regenerate new random numbers every time you win or lose
-//anytime you click on a crystal it should add to toal number
-// create random numbers that the crystals must add up to tubetween 19-12
-//create a way to keep track of wins and loses
-//if number of crysals adds up to a number greater than the number chosen then track loss and restart
-//if number adds up equal to number given than track win
+$(".score-box").append(clickedCrystal)
 
 
